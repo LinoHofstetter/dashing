c6da145a29b99fe4fa8f386a837b0c3cdc433607
Lino Hofstetter <lino@Hugo.fritz.box>
2024-03-30 19:32:15 +0100

    modified Makefile to create a static library

---
+++ src/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+
+namespace bns {
+int dashing_main(int argc, char *argv[]); // Main function declaration, renamed for library usage
+} // namespace bns
+
+#endif // MAIN_H
